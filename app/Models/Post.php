e164906890eee7b0450b1f4b48c0e6206bfdcb66
mahmoud <malmalah04@gmail.com>
2022-10-16 08:14:13 +0200

    Initial commit

---
+++ app/Models/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'name',
+        'slug',
+        'short_description',
+        'description',
+        'regular_price',
+        'sale_price',
+        'SKU',
+        'stock_status',
+        'featured',
+        'quantity',
+        'image',
+        'images',
+        'category_id',
+    ];
+}
---
+++ database/migrations/2022_10_16_035432_create_posts_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('short_description')->nullable();
+            $table->text('description');
+            $table->decimal('regular_price');
+            $table->decimal('sale_price')->nullable();
+            $table->string('SKU');
+            $table->enum('stock_status',['instock','outofstock']);
+            $table->boolean('featured')->default(false);
+            $table->unsignedInteger('quantity')->default(10);
+            $table->string('image')->nullable();
+            $table->text('images')->nullable();
+            $table->bigInteger('category_id')->unsigned()->nullable();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
